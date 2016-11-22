00933a9c884a243b02eb5259ed379fd357409330
umita <umita@10.50.131.41>
2016-11-22 11:27:56 +0100

    commit 1: Fail in makeTestUsers. can't connect db

--- src/main/webapp/app/view_documentation/view_documentation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('myApp.view_documentation', ['ngRoute'])
-
-        .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view_documentation', {
-    templateUrl: 'app/view_documentation/view_documentation.html'
-    
-  });
-}]);
