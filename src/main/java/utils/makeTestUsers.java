12a41442bd7aa93fdd63764c27807caf08078433
PeterBoss <peter2137@gmail.com>
2016-11-22 11:38:25 +0100

    makeTestUsers works now, remember to put your own username/password in persistence.xml

--- src/main/java/utils/makeTestUsers.java
+++ src/main/java/utils/makeTestUsers.java
@@ -14,7 +14,7 @@ public class makeTestUsers {
   //Run this file to setup the users required to use the initial version of the seed
   public static void main(String[] args) {
       System.out.println("trin1 ");
-    EntityManager em = Persistence.createEntityManagerFactory("pu_development").createEntityManager();
+    EntityManager em = Persistence.createEntityManagerFactory("buysell").createEntityManager();
     try {
       System.out.println("Creating TEST Users");
       if (em.find(User.class, "user") == null) {
