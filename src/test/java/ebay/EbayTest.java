e269c6b1b314248c85c0f200cb15f99900cd22c8
PeterBoss <peter2137@gmail.com>
2016-12-08 10:19:46 +0100

    added a rest assured test for the ebay GET

--- src/test/java/ebay/EbayTest.java
+++ src/test/java/ebay/EbayTest.java
@@ -5,34 +5,53 @@
  */
 package ebay;
 
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import io.restassured.parsing.Parser;
+import java.net.MalformedURLException;
+import javax.servlet.ServletException;
+import org.apache.catalina.LifecycleException;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import test.utils.EmbeddedTomcat;
 
 /**
  *
  * @author Peter
  */
 public class EbayTest {
-    
+
+    private static final int SERVER_PORT = 9999;
+    private static final String APP_CONTEXT = "/seed";
+    private static EmbeddedTomcat tomcat;
+
     public EbayTest() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass() {
+    public static void setUpClass() throws ServletException, MalformedURLException, LifecycleException {
+        tomcat = new EmbeddedTomcat();
+        tomcat.start(SERVER_PORT, APP_CONTEXT);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = SERVER_PORT;
+        RestAssured.basePath = APP_CONTEXT;
+        RestAssured.defaultParser = Parser.JSON;
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
+        tomcat.stop();
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -41,12 +60,20 @@ public class EbayTest {
      * Test of sendGet method, of class Ebay.
      */
     @Test
-    public void testSendGet() throws Exception {
-        System.out.println("sendGet");
+    public void testGet() throws Exception {
         Ebay instance = new Ebay();
         String result = instance.findByKeywords("xbox");
         assertTrue(!result.isEmpty());
         // TODO review the generated test code and remove the default call to fail.
     }
-    
+
+    @Test
+    public void testGet2() {
+        given()
+                .contentType("application/json")
+                .when()
+                .get("/api/ebaysearch/test").then()
+                .statusCode(200);
+    }
+
 }
